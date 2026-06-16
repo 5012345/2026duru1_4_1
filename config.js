8149ae9d4dc4eeba26944ab6e3b00da6ddbfe97e
5012345 <okb1331@gmail.com>
2026-06-16 23:42:37 +0900

    좌표 사목 버전2

---
+++ config.js
@@ -0,0 +1,18 @@
+// .gitignore에 config.js를 추가하여 키 노출을 방지하세요
+const CONFIG = {
+  // Firebase 웹 설정 객체
+  FIREBASE_CONFIG: {
+    apiKey: "AIzaSyBtd0WW39hBG-RbL1TfxMSO4vZPjRiOg34",
+    authDomain: "duru1-4-1.firebaseapp.com",
+    databaseURL: "YOUR_FIREBASE_DATABASE_URL",
+    projectId: "duru1-4-1",
+    storageBucket: "duru1-4-1.firebasestorage.app",
+    messagingSenderId: "172898152499",
+    appId: "YOUR_FIREB1:172898152499:web:067d57ebbf8c04ac0c7346ASE_APP_ID",
+    measurementId: "G-T1SRYVBVHG"
+  },
+  // Google AI Studio Gemini API 키
+  GEMINI_API_KEY: "YOUR_GEMINI_API_KEY"
+};
+
+export default CONFIG;
